6188a5c78e427498ccdd8fbe9dca5b2b8f06991a
Jonny Andreola <jonnyandreola@gmail.com>
2015-01-29 23:54:33 -0500

    basic frame switch

--- content/js/main.js
+++ content/js/main.js
@@ -1,4 +1,9 @@
 (function(){
+    var totalLength = $('.frame').length;
+    var increment = 100;
+    var windowHeight = $(window).height();
+    $('body').height(windowHeight + (totalLength * increment));
+
     var video = document.getElementById('video');
     video.play();
 
@@ -7,25 +12,26 @@
     }, 2000);
 
 
-    function nextFrame(frame) {
-        $('.frame-one').removeClass('in');
-        clearInterval(frameOne);
-        video.currentTime = 2;
-        setTimeout(function(){
-            $('.frame-two').addClass('in');
-            setInterval(function(){
-                video.currentTime = 5;
-            }, 4000);
-        }, 3000);
+    function nextFrame(frameNumber) {
+        $('.frame').removeClass('in');
+        $('.frame:nth-child('+frameNumber+')').addClass('in');
+        // clearInterval(frameOne);
+        // video.currentTime = 2;
+        // setTimeout(function(){
+        //     $('.frame-two').addClass('in');
+        //     setInterval(function(){
+        //         video.currentTime = 5;
+        //     }, 4000);
+        // }, 3000);
     }
 
     var lastScrollTop = 0;
     $(window).on('scroll', function(){
         var st = $(this).scrollTop();
         if (st > lastScrollTop){
-            if(st > 1 && st < 200) {
-                nextFrame(2);
-            }
+            var frame = st / increment;
+            frame = Math.ceil(frame);
+            nextFrame(frame);
         } else {
           // upscroll code
         }
